1faef44d0621a639843810118f5a9dae495a6e27
Sebastian Agudelo <sebastamriel@gmail.com>
2025-06-22 14:53:14 -0500

    add command comment

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -119,6 +119,7 @@ const Hero = () => {
                 </div>
             </section>
 
+            {/* ffmpeg -i input.mp4 -vf scale=960:-1 -movflags faststart -vcodec libx264 -crf 20 -g 1 -pix_fmt yuv420p output.mp4 */}
             <div className="video absolute inset-0">
                 <video 
                     ref={videoRef}
